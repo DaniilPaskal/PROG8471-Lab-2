66928b971fa7a97e39fffbd71f79296b40a7372c
Daniil Paskal <pask.daniil@gmail.com>
2024-01-17 19:24:14 -0500

    Complete app according to instructions

--- Lab 2/ViewController.swift
+++ Lab 2/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var screenNumber:Int = 0
+    var step:Int = 1
 
+    @IBOutlet weak var numberText: UILabel!
+    
+    @IBAction func buttonInc(_ sender: Any) {
+        screenNumber += step
+        numberText.text = String(screenNumber)
+    }
+    @IBAction func buttonDec(_ sender: Any) {
+        screenNumber -= step
+        numberText.text = String(screenNumber)
+    }
+    
+    @IBAction func buttonReset(_ sender: Any) {
+        screenNumber = 0
+        step = 1
+        numberText.text = String(screenNumber)
+    }
+    @IBAction func buttonStep(_ sender: Any) {
+        step = 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        screenNumber = Int(numberText.text!) ?? 0
+        
         // Do any additional setup after loading the view.
     }
-
-
 }
 
